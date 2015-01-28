0354fa725ede61477032ed33f72939f18f4d41df
chris-marie <cmhamilton2.718@gmail.com>
2015-01-27 20:08:32 -0800

    tried to clean - still amiss

--- simplebounceEx.js
+++ simplebounceEx.js
@@ -8,18 +8,12 @@ document.addEventListener('DOMContentLoaded', function () {
     var miny = ballBox.clientTop;
     var maxy = ballBox.clientHeight + ballBox.clientTop;
 
+    // CURRENTLY ONLY A GIVES THE OPPOSITE DIRECTION - LATER WILL CALCULATE ANGLE
     var directionObject = {
         'right': 'left',
         'left': 'right',
         'up': 'down',
         'down': 'up'
-    };
-
-    var incrementObject = {
-        'right': increment,
-        'left': decrement,
-        'up': increment,
-        'down': decrement
     };
 
     ball.addEventListener('click', function () {
@@ -29,63 +23,88 @@ document.addEventListener('DOMContentLoaded', function () {
         var ydir = 'down';
 
         var intervalid = setInterval(function () {
-            bounce(x,y);
+            bounce();
 
-        },100);
+        },500);
 
-        function bounce (x,y) {
+        function bounce () {
             x = ball.clientLeft;
             y = ball.clientTop;
-            console.log('bouncing');
+
+            // CHECKS IF THE x WILL GO OUT OF BOUNDS AND INCREMENTS
             if(inBounds(xdir,x)) {
-                console.log(incrementObject[xdir],',',incrementObject[xdir](x));
-                incrementObject[xdir].call(x);
+                x = getPosition(xdir,x);
+                console.log('IN x: ', x);
             } else if (!inBounds (xdir,x)){
                 xdir = switchDirections(xdir);
-                incrementObject[xdir](x);
+                x = getPosition(xdir,x);
+                console.log('SWITCH x: ',x);
             }
 
+            // CHECKS IF THE y WILL GO OUT OF BOUNDS AND INCREMENTS
             if(inBounds(ydir,y)) {
-                incrementObject[ydir](y);
+                y = getPosition(ydir,y);
+                console.log('IN y: ', y);
             } else if (!inBounds(ydir,y)) {
                 ydir = switchDirections(ydir);
-                incrementObject[ydir](y);
+                y = getPosition(ydir,y);
+                console.log('SWITCH y: ',y);
             }
             console.log('moving to: ', x,',',y);
+
+            //  ASSIGNS TO THE ball ELEMENT
             ball.style.left = x;
             ball.style.top = y;
-
-
         }
 
     });
 
+    function getPosition (direction, pos) {
+        if(direction == 'right' || direction == 'down') {
+            var pos2 = increment(pos);
+            console.log('inc OLD,NEW: ',pos,',',pos2);
+            return pos2;
+        } else if(direction == 'left' || direction == 'up') {
+            var pos2 = decrement(pos);
+            console.log('dec OLD,NEW: ',pos,',',pos2);
+            return pos2;
+        }
+    }
 
-
+    // REASON: CURRENTLY JUST USES THE CURRENT DIRECTION AS A KEY TO GET THE OPPOSITE DIRECTION
+    // LATER THIS WILL GET THE ANGLE
     function switchDirections (dirKey) {
         return directionObject[dirKey];
     }
+
+
+    // REASON: I WANTED TO KEEP THESE SEPARATE FUNCTIONS
+    // BECAUSE I WILL ADD GRAVITY/VELOCITY FACTORS TO THE INCREMENT LATER
     function increment (pos) {
-        console.log('incrementing');
-        pos ++;
+        return pos + 10;
     }
     function decrement(pos) {
-        console.log('decrement');
-        pos--;
+        return pos - 10;
     }
+
+    // REASON: CHECKS THE BOUNDS DEPENDING ON THE DIRECTION TRAVELING
     function inBounds (direction,pos) {
         switch (direction) {
             case 'right':
+                console.log('inBounds right');
                 return pos < maxx;
                 break;
             case 'left':
+                console.log('inBounds left');
                 return pos > minx;
                 break;
             case 'up':
-                return pos < maxy;
+                console.log('inBounds up');
+                return pos > miny;
                 break;
             case 'down':
-                return pos > miny;
+                console.log('inBounds down');
+                return pos < maxy;
                 break;
             default:
                 console.log('ERROR on inbounds dir,pos: ', direction,',',pos);
